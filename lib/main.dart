60ce54b21e0a74da5ff275f4c7e0d6fc629db54f
RitwikSingh28 <itsritwik26@gmail.com>
2023-01-12 12:00:27 +0530

    add firebase config

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,27 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:insta_clone/responsive/mobile_screen_layout.dart';
 import 'package:insta_clone/responsive/responsive_layout_screen.dart';
 import 'package:insta_clone/responsive/web_screen_layout.dart';
 import 'package:insta_clone/utils/colors.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding
+      .ensureInitialized(); //ensure that the Flutter widgets get
+
+  if (kIsWeb) {
+    await Firebase.initializeApp(
+        options: const FirebaseOptions(
+      apiKey: "AIzaSyDozCsmQCPYNY_GC2QRB4tyi_v7QrM9DHI",
+      appId: "1:401313714071:web:a06eaf72f77032de2e3ed2",
+      messagingSenderId: "401313714071",
+      projectId: "instaclone-bf20c",
+      storageBucket: "instaclone-bf20c.appspot.com",
+    ));
+  } else {
+    await Firebase.initializeApp();
+  }
   runApp(const MyApp());
 }
 
