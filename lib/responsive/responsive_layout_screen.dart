5ffc66a828e616c16934202d8cb3b748abe6aaa8
RitwikSingh28 <itsritwik26@gmail.com>
2023-01-12 11:09:41 +0530

    structure project

---
+++ lib/responsive/responsive_layout_screen.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:insta_clone/utils/dimensions.dart';
+
+class ResponsiveLayout extends StatelessWidget {
+  final Widget webScreenLayout;
+  final Widget mobileScreenLayout;
+  const ResponsiveLayout(
+      {Key? key,
+      required this.mobileScreenLayout,
+      required this.webScreenLayout})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return LayoutBuilder(builder: (context, constraints) {
+      if (constraints.maxWidth > webScreenSize) {
+        //web screen layout
+        return webScreenLayout;
+      } else {
+        //mobile screen layout
+        return mobileScreenLayout;
+      }
+    });
+  }
+}
