dab2ee66411489fd30954e11bc0c5a5a1f9ef334
RitwikSingh28 <itsritwik26@gmail.com>
2023-01-14 20:09:33 +0530

    add bottomNav

--- lib/responsive/mobile_screen_layout.dart
+++ lib/responsive/mobile_screen_layout.dart
@@ -1,6 +1,9 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:insta_clone/models/user_model.dart';
 import 'package:insta_clone/providers/user_provider.dart';
+import 'package:insta_clone/screens/add_posts_screen.dart';
+import 'package:insta_clone/utils/colors.dart';
 import 'package:provider/provider.dart';
 
 class MobileScreen extends StatefulWidget {
@@ -11,14 +14,83 @@ class MobileScreen extends StatefulWidget {
 }
 
 class _MobileScreenState extends State<MobileScreen> {
+  int _page = 0;
+  late PageController _pageController;
+
+  @override
+  void initState() {
+    super.initState();
+    _pageController = PageController();
+  }
+
+  @override
+  void dispose() {
+    _pageController.dispose();
+    super.dispose();
+  }
+
+  void navigationTapped(int page) {
+    _pageController.jumpToPage(page);
+  }
+
   @override
   Widget build(BuildContext context) {
     CustomUser user = Provider.of<UserProvider>(context).getUser;
     return Scaffold(
-      body: Scaffold(
-        body: Center(
-          child: Text(user.userName),
-        ),
+      body: PageView(
+        controller: _pageController,
+        onPageChanged: (value) => setState(() {
+          _page = value;
+        }),
+        physics: const NeverScrollableScrollPhysics(),
+        children: const [
+          Text('Home'),
+          Text('Search'),
+          AddPost(),
+          Text('Notifications'),
+          Text('Profile'),
+        ],
+      ),
+      bottomNavigationBar: CupertinoTabBar(
+        backgroundColor: mobileBackgroundColor,
+        items: [
+          BottomNavigationBarItem(
+              icon: Icon(
+                Icons.home,
+                color: _page == 0 ? primaryColor : secondaryColor,
+              ),
+              label: '',
+              backgroundColor: primaryColor),
+          BottomNavigationBarItem(
+              icon: Icon(
+                Icons.search,
+                color: _page == 1 ? primaryColor : secondaryColor,
+              ),
+              label: '',
+              backgroundColor: primaryColor),
+          BottomNavigationBarItem(
+              icon: Icon(
+                Icons.add_circle,
+                color: _page == 2 ? primaryColor : secondaryColor,
+              ),
+              label: '',
+              backgroundColor: primaryColor),
+          BottomNavigationBarItem(
+              icon: Icon(
+                Icons.favorite,
+                color: _page == 3 ? primaryColor : secondaryColor,
+              ),
+              label: '',
+              backgroundColor: primaryColor),
+          BottomNavigationBarItem(
+              icon: Icon(
+                Icons.person,
+                color: _page == 4 ? primaryColor : secondaryColor,
+              ),
+              label: '',
+              backgroundColor: primaryColor),
+        ],
+        onTap: navigationTapped,
       ),
     );
   }
---
+++ lib/screens/add_posts_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class AddPost extends StatelessWidget {
+  const AddPost({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: IconButton(
+        icon: const Icon(Icons.upload),
+        onPressed: () {},
+      ),
+    );
+  }
+}
